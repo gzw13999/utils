4f038062f33daeff9a649a3743eb9ffc0c2ee232
gzw13999 <gzw13999@126.com>
2020-05-01 03:00:41 +0800

    add verify
    Signed-off-by: gzw13999 <gzw13999@126.com>

---
+++ verify.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"regexp"
+)
+
+//匹配电子邮箱
+func VerifyEmail(email string) bool {
+	pattern := `\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*`
+	reg := regexp.MustCompile(pattern)
+	return reg.MatchString(email)
+}
