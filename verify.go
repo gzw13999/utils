ec863147350010c8e6b0f875c3e6b9cde333528f
gzw13999 <gzw13999@126.com>
2020-05-29 21:28:41 +0800

    Signed-off-by: gzw13999 <gzw13999@126.com>

--- verify.go
+++ verify.go
@@ -16,3 +16,33 @@ func VerifyPhone(s string) bool {
 	reg := regexp.MustCompile(`^1(3|4|5|6|7|8)\d{9}$`)
 	return reg.MatchString(s)
 }
+
+//匹配密码
+func VerifyPassword(s string) bool {
+
+	reg := regexp.MustCompile("^[a-zA-Z0-9-*/+.~!@#$%^&*()]{6,20}$")
+	return reg.MatchString(s)
+}
+
+func IsGuid(s string) bool {
+	if len(s) != 36 {
+		return false
+	}
+
+	reg := regexp.MustCompile("^[a-fA-F0-9]{8}(-[a-fA-F0-9]{4}){3}-[a-fA-F0-9]{12}$")
+	if reg.MatchString(s) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func IsNumber(s string) bool {
+	reg := regexp.MustCompile("^[0-9]+$")
+	return reg.MatchString(s)
+}
+
+func IsABC(s string) bool {
+	reg := regexp.MustCompile("^[a-zA-Z]+$")
+	return reg.MatchString(s)
+}
