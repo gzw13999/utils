6c0564a1a506bbe99ddb74005483b4912f628cba
gzw13999 <gzw13999@126.com>
2020-07-29 18:30:07 +0800

    Signed-off-by: gzw13999 <gzw13999@126.com>

--- compress.go
+++ compress.go
@@ -5,9 +5,9 @@ import (
 	"compress/gzip"
 )
 
-func Gzip(b []byte) ([]byte, error) {
+func Gzip(b []byte,level int) ([]byte, error) {
 	var buf bytes.Buffer
-	if gzw, err := gzip.NewWriterLevel(&buf, gzip.BestCompression); err != nil {
+	if gzw, err := gzip.NewWriterLevel(&buf, level); err != nil {
 		return nil, err
 	} else {
 		if _, err := gzw.Write(b); err != nil {
