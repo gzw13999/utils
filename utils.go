7b386f13169bda8fb0349464d8ec96704bf23ffb
gzw13999 <gzw13999@126.com>
2024-04-07 00:49:28 +0800

    add func GetHourDiffer

--- utils.go
+++ utils.go
@@ -212,3 +212,17 @@ func ToFixed(f float64, places int) float64 {
 	//return math.Floor(fv*shift+.5) / shift //四舍五入
 	return math.Floor(fv*shift) / shift
 }
+
+// GetHourDiffer 获取相差时间
+func GetHourDiffer(startTime, endTime string) int64 {
+	var hour int64
+	t1, err := time.ParseInLocation("2006-01-02 15:04:05", startTime, time.Local)
+	t2, err := time.ParseInLocation("2006-01-02 15:04:05", endTime, time.Local)
+	if err == nil && t1.Before(t2) {
+		diff := t2.Unix() - t1.Unix() //
+		hour = diff / 3600
+		return hour
+	} else {
+		return hour
+	}
+}
